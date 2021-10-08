60740c616f925022511cc164c4e7e931ea2ada6c
Romain Penchenat <romain.penchenat@gmail.com>
2021-10-09 00:12:26 +0200

    Initial Commit

---
+++ BoxFinder/BoxFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxFinderApp.swift
+//  BoxFinder
+//
+//  Created by Romain Penchenat on 09/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct BoxFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
